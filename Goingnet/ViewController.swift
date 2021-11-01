82162cf6f18ae2b704f21c5a86706c16d9f638de
Yu <yugochen711@gmail.com>
2021-11-01 16:52:36 +0800

    Initial Commit

---
+++ Goingnet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goingnet
+//
+//  Created by 陳冠宇 on 2021/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoingnetTests/GoingnetTests.swift
@@ -0,0 +1,33 @@
+//
+//  GoingnetTests.swift
+//  GoingnetTests
+//
+//  Created by 陳冠宇 on 2021/11/1.
+//
+
+import XCTest
+@testable import Goingnet
+
+class GoingnetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
